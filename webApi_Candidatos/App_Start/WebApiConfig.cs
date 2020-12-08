6ebf139cbe1c72a85bf18aa9f31cec3b8b6fd029
Anderson martins <andersonmartins44@yahoo.com.br>
2020-12-08 20:24:38 -0300

    esta parte foi adicionada para a resposta fica no formato JSON e pode visualizar no navegador

--- webApi_Candidatos/App_Start/WebApiConfig.cs
+++ webApi_Candidatos/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace webApi_Candidatos
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
         }
     }
 }
