af8eebfed3e499396c97946badfb117bacc81ee5
Anderson martins <andersonmartins44@yahoo.com.br>
2020-12-08 19:51:13 -0300

    Interface definimos os métodos que deverá ser implementados

--- webApi_Candidatos/Models/Produto.cs
+++ webApi_Candidatos/Models/Produto.cs
@@ -7,5 +7,9 @@ namespace webApi_Candidatos.Models
 {
     public class Produto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
     }
 }
